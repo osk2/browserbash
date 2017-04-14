bb1665f9319976e57553692b463fba942a5e65cd
osk2 <sc60714@gmail.com>
2017-04-14 14:54:28 +0800

    Complete basic terminal

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
 const express = require('express');
 const app = express();
+const pty = require('pty.js');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const spawn = require('child_process').spawn;
-const sh = spawn('bash');
+const term = pty.spawn('bash', [], {
+  name: 'xterm-color',
+  cols: 100,
+  rows: 40,
+  cwd: process.env.HOME,
+  env: process.env
+});
 
-app.use(express.static('./'))
+app.use(express.static('./'));
 
-sh.stdout.on('data', function(data) {
+term.on('data', function(data) {
   io.emit('message', data);
 });
 
-sh.stderr.on('data', function(data) {
-  io.emit('message', data);
-});
+io.on('connection', function (client) {
 
-sh.on('exit', function (code) {
-  io.emit('exit', '** Shell exited: '+code+' **');
-});
+  client.on('message', function (data) {
+    if (/\t/.test(data)) {
+      term.write(data + '\t');
+      return;
+    }
+    if (/sudo |su /.test(data)) {
+      io.emit('message', 'No sudo please O_o');
+      return;
+    }
+    if (/^clear$/.test(data)) {
+      io.emit('clear');
+      term.write('clear\r');
+      return;
+    }
+    term.write(data + '\r');
+  });
 
-io.on('connection', function(client){
-  client.on('message', function(data){
-    sh.stdin.write(data+"\n");
-    io.emit('message', new Buffer("> "+data));
+  client.on('typing', function (data) {
+    io.emit('typing', data);
+  });
+
+  client.on('kill', function () {
+    term.kill('SIGINT');
   });
 });
 
-server.listen(8080, function(){
-  console.log('server started');
-})
+server.listen(8080, function () {
+  console.log('App listening on port 8080');
+});
